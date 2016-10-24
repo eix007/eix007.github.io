21dd74a46e2d098d1b21157dc478888480a62e74
eix007 <anas.alabtah@gmail.com>
2016-10-24 03:37:10 -0700

    upload profile

---
+++ firstjs.js
@@ -0,0 +1,11 @@
+var massge = "This is first function";
+console.log(massge);
+
+onload = function () {
+    document.getElementById("about").innerHTML = "About Me";
+	document.getElementById("Education").innerHTML = "My Education";
+};
+
+function TheDate() {
+    document.getElementById("date").innerHTML = Date();
+};
